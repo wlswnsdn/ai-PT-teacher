f0955032d358fa81e149c5b8039284acfdb67c68
wlswnsdn <wlswnsdn123@gmail.com>
2024-04-30 15:08:46 +0900

    user/avatar 테이블 완성, 테스트 완

--- app/src/main/java/org/androidtown/gympalai/database/GymPalDB.java
+++ app/src/main/java/org/androidtown/gympalai/database/GymPalDB.java
@@ -5,14 +5,24 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import org.androidtown.gympalai.converter.Converters;
+import org.androidtown.gympalai.dao.AvatarDao;
+import org.androidtown.gympalai.dao.UserDao;
 import org.androidtown.gympalai.entity.Avatar;
 import org.androidtown.gympalai.entity.User;
-
+// 데이터베이스에 넣을 테이블 추가,
 @Database(entities = {User.class, Avatar.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class GymPalDB extends RoomDatabase {
 
     private static GymPalDB instance = null;
+
+    // 테이블의 Dao 추가
+    public abstract UserDao userDao();
+
+    public abstract AvatarDao avatarDao();
 
     public static GymPalDB getInstance(Context context) {
         if (instance == null) {
