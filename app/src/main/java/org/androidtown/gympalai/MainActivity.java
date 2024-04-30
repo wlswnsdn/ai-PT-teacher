5b0b831cc14526ab3db048b51c4ce07bdf5887fd
SHARDEN\김석영 <sam010808@naver.com>
2024-04-30 18:27:34 +0900

    basic layer 네비게이션 바, fragment 틀 생성

--- app/src/main/java/org/androidtown/gympalai/MainActivity.java
+++ app/src/main/java/org/androidtown/gympalai/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationBarView;
+
 import org.androidtown.gympalai.database.GymPalDB;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
