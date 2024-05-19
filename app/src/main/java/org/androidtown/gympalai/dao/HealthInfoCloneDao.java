766cd018864b19e54e444b36418bc0055ca08249
chanu.develop@gmail.com <chanu.develop@gmail.com>
2024-05-19 12:51:46 +0900

    그래프 그리기 위한 HealthInfo 엔티티 복제본 생성

---
+++ app/src/main/java/org/androidtown/gympalai/dao/HealthInfoCloneDao.java
@@ -0,0 +1,24 @@
+package org.androidtown.gympalai.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import org.androidtown.gympalai.entity.HealthInfoClone;
+
+import java.util.List;
+
+public interface HealthInfoCloneDao {
+
+    @Insert
+    void insert (HealthInfoClone healthInfoClone);
+    @Update
+    void update (HealthInfoClone healthInfoClone);
+    @Delete
+    void delete (HealthInfoClone healthInfoClone);
+    @Query("SELECT * FROM healthInfoClone WHERE userID = :userId")
+    LiveData<List<HealthInfoClone>> getUserInfoList(String userId); // 그래프 만들 떄 사용
+
+}
